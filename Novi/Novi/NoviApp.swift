0485bb09cc7a4814561348be8fead128215e296b
Arjang Khademi <arjangkhdm@gmail.com>
2026-06-06 10:38:06 +0200

    First Commit

---
+++ Novi/Novi/NoviApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoviApp.swift
+//  Novi
+//
+//  Created by Arjang Khademi on 06.06.26.
+//
+
+import SwiftUI
+
+@main
+struct NoviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
